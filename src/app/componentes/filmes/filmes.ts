21015bcf45c8ff3647257e808e6008b77535bfe1
William Firmino <prof.williamfirmino@gmail.com>
2025-07-08 21:34:47 -0300

    Enviando alterações do dia 08/07/2025

---
+++ src/app/componentes/filmes/filmes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filmes',
+  imports: [],
+  templateUrl: './filmes.html',
+  styleUrl: './filmes.css'
+})
+export class Filmes {
+
+}
