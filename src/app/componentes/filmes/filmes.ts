ab5f721ef5bb58d626db36274e628cce322067c0
William Firmino <prof.williamfirmino@gmail.com>
2025-07-10 19:51:43 -0300

    Alterações de antes do intervalo, 10/07/2025

--- src/app/componentes/filmes/filmes.ts
+++ src/app/componentes/filmes/filmes.ts
@@ -7,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrl: './filmes.css'
 })
 export class Filmes {
+  filme1 = {
+    nome: "Titanic",
+    imagem: "/titanic.jpg",
+    sinopse: "Filme emocionante de um navio que afunda."
+  }
+  filme2 = {
+    nome: "Origem",
+    imagem: "/origem.jpg",
+    sinopse: "Filme emocionante de um navio que afunda."
+  }
+  filme3 = {
+    nome: "Premonicao",
+    imagem: "/premonicao.jpg",
+    sinopse: "Filme emocionante de um navio que afunda."
+  }
+
 
 }
