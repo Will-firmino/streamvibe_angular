35d464636d0c6ccc4b9095069dba3c68a391c3ac
William Firmino <prof.williamfirmino@gmail.com>
2025-07-10 21:39:26 -0300

    Alterações finais do dia 10/07/2025

--- src/app/componentes/filmes/filmes.ts
+++ src/app/componentes/filmes/filmes.ts
@@ -7,21 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './filmes.css'
 })
 export class Filmes {
+
+  alternarFavorito() {
+    this.filme1.favorito = !this.filme1.favorito
+  }
+
   filme1 = {
     nome: "Titanic",
     imagem: "/titanic.jpg",
-    sinopse: "Filme emocionante de um navio que afunda."
+    sinopse: "Filme emocionante de um navio que afunda.",
+    favorito: false
   }
-  filme2 = {
-    nome: "Origem",
-    imagem: "/origem.jpg",
-    sinopse: "Filme emocionante de um navio que afunda."
-  }
-  filme3 = {
-    nome: "Premonicao",
-    imagem: "/premonicao.jpg",
-    sinopse: "Filme emocionante de um navio que afunda."
-  }
+  // filme2 = {
+  //   nome: "Origem",
+  //   imagem: "/origem.jpg",
+  //   sinopse: "Filme emocionante de um navio que afunda."
+  // }
+  // filme3 = {
+  //   nome: "Premonicao",
+  //   imagem: "/premonicao.jpg",
+  //   sinopse: "Filme emocionante de um navio que afunda."
+  // }
 
 
 }
